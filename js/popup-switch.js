ea714a07e2d3f29bdf865f84dc19fc351bc2f94b
Lisi4ka <kirich44@yandex.ru>
2021-10-19 19:52:47 +0500

    Поправь классы

--- js/popup-switch.js
+++ js/popup-switch.js
@@ -1,28 +1,31 @@
 const adForm = document.querySelector('.ad-form');
 const adFormFieldsets = adForm.querySelectorAll('.ad-form__element');
 const formFilters = document.querySelector('.map__filters');
-const formFilterFields = adForm.querySelectorAll('.map__filter' && '.map__features');
+const formFilterFields = formFilters.querySelectorAll('.map__filter');
+const formFilterField = formFilters.querySelector('.map__features');
 
 const getFormActive = () => {
-  adForm.classList.remove('.ad-form--disabled');
-  formFilters.classList.remove('.map__filters--disabled');
+  adForm.classList.remove('ad-form--disabled');
+  formFilters.classList.remove('map__filters--disabled');
   adFormFieldsets.forEach((adFormFieldset) => {
     adFormFieldset.removeAttribute('disabled', '');
   });
-  formFilterFields.forEach((formFilterField) => {
-    formFilterField.removeAttribute('disabled', '');
+  formFilterFields.forEach((formFilterFieldElement) => {
+    formFilterFieldElement.removeAttribute('disabled', '');
   });
+  formFilterField.removeAttribute('disabled', '');
 };
 
 const getFormInactive = () => {
-  adForm.classList.add('.ad-form--disabled');
-  formFilters.classList.add('.map__filters--disabled');
+  adForm.classList.add('ad-form--disabled');
+  formFilters.classList.add('map__filters--disabled');
   adFormFieldsets.forEach((adFormFieldset) => {
     adFormFieldset.setAttribute('disabled', '');
   });
-  formFilterFields.forEach((formFilterField) => {
-    formFilterField.setAttribute('disabled', '');
+  formFilterFields.forEach((formFilterFieldElement) => {
+    formFilterFieldElement.setAttribute('disabled', '');
   });
+  formFilterField.setAttribute('disabled', '');
 };
 
 getFormInactive();
