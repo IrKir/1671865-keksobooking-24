1e0a62eae0b51a3c82154fd5cf4dbf41f9eb017b
Lisi4ka <kirich44@yandex.ru>
2021-10-25 12:55:47 +0500

    Проверь все поля объявления на наличие

--- js/card.js
+++ js/card.js
@@ -1,5 +1,3 @@
-import {finalArray} from './data.js';
-
 const HOUSING_TYPE = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
@@ -8,18 +6,33 @@ const HOUSING_TYPE = {
   hotel: 'Отель',
 };
 
+const checkForAvailability = (value, element) => value || element.classList.add('visually-hidden');
+
+const cardTemplate = document.querySelector('#card').content;
+const popup = cardTemplate.querySelector('.popup');
+
 const createCustomPopup = (element) => {
-  const cardTemplate = document.querySelector('#card').content;
-  const popup = cardTemplate.querySelector('.popup');
   const popupClone = popup.cloneNode(true);
-  popupClone.querySelector('.popup__title').textContent = element.offer.title;
-  popupClone.querySelector('.popup__text--address').textContent = element.offer.address;
-  popupClone.querySelector('.popup__text--price').textContent = `${element.offer.price  } ₽/ночь`;
 
-  popupClone.querySelector('.popup__type').textContent = HOUSING_TYPE[element.offer.type];
+  const popupTitle = popupClone.querySelector('.popup__title');
+  const popupAddress = popupClone.querySelector('.popup__text--address');
+  const popupPrice = popupClone.querySelector('.popup__text--price');
+  const popupType = popupClone.querySelector('.popup__type');
+  const popupCapacity = popupClone.querySelector('.popup__text--capacity');
+  const popupTime = popupClone.querySelector('.popup__text--time');
+  const popupDescription = popupClone.querySelector('.popup__description');
+  const popupPhotos = popupClone.querySelector('.popup__photos');
+  const popupPhoto = popupClone.querySelector('.popup__photo');
+  const popupAvatar = popupClone.querySelector('.popup__avatar');
 
-  popupClone.querySelector('.popup__text--capacity').textContent = `${element.offer.rooms  } комнаты для ${  element.offer.guests  } гостей`;
-  popupClone.querySelector('.popup__text--time').textContent = `Заезд после ${  element.offer.checkin  }, выезд до ${  element.offer.checkout}`;
+
+  popupTitle.textContent = checkForAvailability(element.offer.title, popupTitle);
+  popupAddress.textContent = checkForAvailability(element.offer.address, popupAddress);
+  popupPrice.textContent = `${checkForAvailability(element.offer.price, popupPrice)} ₽/ночь`;
+  popupType.textContent = checkForAvailability(HOUSING_TYPE[element.offer.type], popupType);
+  popupCapacity.textContent = `${checkForAvailability(element.offer.rooms, popupCapacity)} комнаты для ${checkForAvailability(element.offer.guests, popupCapacity)} гостей`;
+  popupTime.textContent = `Заезд после ${checkForAvailability(element.offer.checkin, popupTime)}, выезд до ${checkForAvailability(element.offer.checkout, popupTime)}`;
+
   const userFeatures = element.offer.features;
   const featuresContainer = popupClone.querySelector('.popup__features');
   const featuresList = featuresContainer.querySelectorAll('.popup__feature');
@@ -31,23 +44,21 @@ const createCustomPopup = (element) => {
       featuresListItem.remove();
     }
   });
+  popupDescription.textContent = checkForAvailability(element.offer.description, popupDescription);
 
-  if (!element.offer.description) {
-    popupClone.querySelector('.popup__description').classList.add('visually-hidden');
-  } else {
-    popupClone.querySelector('.popup__description').textContent = element.offer.description;
-  }
-  const popupPhotos = popupClone.querySelector('.popup__photos');
+
   popupPhotos.innerHTML = '';
   element.offer.photos.forEach((photoItem) => {
-    const img = document.createElement('img');
-    img.classList.add('popup__photo');
-    img.src = photoItem;
-    img.width = 45;
-    img.height = 40;
-    popupPhotos.appendChild(img);
+    const photo = popupPhoto.cloneNode(true);
+    if (photoItem) {
+      photo.src = photoItem;
+      popupPhotos.append(photo);
+    } else {
+      popupPhotos.classList.add('visually-hidden');
+      photo.alt ='';
+    }
   });
-  popupClone.querySelector('.popup__avatar').src = element.author.avatar;
+  popupAvatar.src = checkForAvailability(element.author.avatar, popupAvatar);
 
   return popupClone;
 };
