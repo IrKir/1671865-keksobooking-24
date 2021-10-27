3af68ffccb19e9ad5595ebe4a4f5b1a08ea85a88
Lisi4ka <kirich44@yandex.ru>
2021-10-27 20:28:13 +0500

    Удали вызов функции

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 import {onDwellingChange, setAdFormValidation} from'./form-validation.js';
 import {deactivateForm} from './page-switch.js';
-import {setMap, setPins, setMainMarkerAddress} from './map.js';
+import {setMap, setMainMarkerAddress} from './map.js';
 import './api.js';
 
 onDwellingChange();
 setAdFormValidation();
 deactivateForm();
 setMap();
-setPins();
 setMainMarkerAddress();
