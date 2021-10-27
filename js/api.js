0948d314f6dec732673c36e9cee79e36a6cff92e
Lisi4ka <kirich44@yandex.ru>
2021-10-27 20:29:46 +0500

    Создай модуль для работы с сервером

---
+++ js/api.js
@@ -0,0 +1,31 @@
+import {setPins} from './map.js';
+
+const getData = fetch('https://24.javascript.pages.academy/keksobooking/data',
+  {
+    method: 'GET',
+  },
+)
+  .then((response) => response.json())
+  .then((advertisements) => {
+    setPins(advertisements);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+/*const setData = fetch('https://24.javascript.pages.academy/keksobooking/data',
+  {
+    method: 'POST',
+    body: new FormData(),
+  },
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Результат', data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });*/
+
+
+export {getData};
