a47aac8f055bc351844288389beabf36affc3ac3
Lisi4ka <kirich44@yandex.ru>
2021-11-05 12:37:28 +0500

    Удали лишний параметр

--- js/page-switch.js
+++ js/page-switch.js
@@ -1,19 +1,19 @@
 const adForm = document.querySelector('.ad-form');
 const adFormFieldsets = adForm.querySelectorAll('.ad-form__element');
 const filterForm = document.querySelector('.map__filters');
-const filterFormFields =  filterForm.querySelectorAll('.map__filter');
-const filterFormField =  filterForm.querySelector('.map__features');
+const filterFormFields = filterForm.querySelectorAll('.map__filter');
+const filterFormField = filterForm.querySelector('.map__features');
 
 const activateForm = () => {
   adForm.classList.remove('ad-form--disabled');
   filterForm.classList.remove('map__filters--disabled');
   adFormFieldsets.forEach((adFormFieldset) => {
-    adFormFieldset.removeAttribute('disabled', '');
+    adFormFieldset.removeAttribute('disabled');
   });
   filterFormFields.forEach((filterFormFieldElement) => {
-    filterFormFieldElement.removeAttribute('disabled', '');
+    filterFormFieldElement.removeAttribute('disabled');
   });
-  filterFormField.removeAttribute('disabled', '');
+  filterFormField.removeAttribute('disabled');
 };
 
 const deactivateForm = () => {
